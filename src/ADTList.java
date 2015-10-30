f48cfd656fe0f1d2c67ebee685cea47052d5a11a
kobed6328 <kobed6328@HRH-IC0027337.wrdsb.ca>
2015-10-30 11:12:43 -0400

    Wow

--- src/ADTList.java
+++ src/ADTList.java
@@ -8,18 +8,37 @@
  * @author kobed6328
  */
 public class ADTList {
-    private int[] array = new int[0];
+    private int[] array = new int[10];
+    int limit = 0;
     
     public void add(int index, int num)
     {
+        if (limit == array.length)
+        {
+            expandArray();
+        }
+        if (index > limit)
+        {
+            index = limit;
+        }
+        limit ++;
         
+    }
+    
+    private void expandArray()
+    {
+        int[] newArray = new int[array.length*2];
+        for (int i = 0; i < array.length; i ++)
+        {
+            newArray[i] = array[i];
+        }
     }
     
     public void print()
     {
-        for (Integer i: array)
+        for (int i = 0; i < limit; i ++)
         {
-            System.out.print(i);
+            System.out.print(array[i]);
         }
         System.out.println("");
     }
