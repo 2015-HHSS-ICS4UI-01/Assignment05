6c81ea2b9f1cf87a11a3f25c2aca97b2026d0e5e
kobed6328 <kobed6328@HRH-IC0027337.wrdsb.ca>
2015-11-04 11:13:05 -0500

    Done commenting and done assignment.. gonna test it later

--- src/Main.java
+++ src/Main.java
@@ -14,8 +14,61 @@ public class Main {
      */
     public static void main(String[] args) {
         
-        ADTList list = new ADTList();
-        list.print();
-        
+    }
+    
+    /**
+     * Returns whether or not a string is in the form of "[char]$[char]"
+     * @param str the string to be checked
+     * @return true if the string is in the proper format; otherwise, return false
+     */
+    public static boolean isValidString(String str)
+    {
+        // a stack is going to be used to compare the part before the $ and the part after
+         ADTStack stack = new ADTStack();
+         
+         // the dollar sign has not yet been reached
+         boolean doneAdding = false;
+         
+         for (int i = 0; i < str.length(); i ++)
+         {
+             // if the dollar sign has been reached, doneAdding is set to true, and the loop goes to the next iteration
+             if (str.charAt(i) == '$')
+             {
+                 doneAdding = true;
+             }
+             // if the dollar sign has not yet been reached
+             else if (!doneAdding)
+             {
+                 // add the current element to the stack (this reverses the order of the part 
+                    //before the dollar sign, making it comparable to the part after
+                 stack.add(new CharNode(str.charAt(i)));
+             }
+             // if everything has been added, compare the last added character in the 
+                // stack to the next character in the string
+             else
+             {
+                 /* if the stack is empty but the end of the string has not yet been reached,
+                  *     the last part is longer than the first part and the string is not valid
+                  * 
+                  * otherwise, if the two characters do not match, the string is invalid
+                  */  
+                 if (stack.isEmpty() || stack.pop().getValue() != str.charAt(i))
+                 {
+                     return false;
+                 }
+             }
+         }
+         
+         /*
+          * if the end of the string has been reached but the stack is not yet empty,
+          *     then the fist part is longer than the last part, and the string is invalid
+          */
+         if (!stack.isEmpty())
+         {
+             return false;
+         }
+         
+         // at this point, the string is definitely valid
+         return true;
     }
 }
