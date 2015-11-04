3d47448aafeaacac58f42f783a284dfd878f7f8a
donet6376 <donet6376@HRH-IC0023631.wrdsb.ca>
2015-11-04 14:22:15 -0500

    A5Q1 Done and A5Q2 Add Method Done

---
+++ src/Testing.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author donet6376
+ */
+public class Testing {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        A5Q2 list = new A5Q2();
+        
+        list.add(0,8);
+        list.add(0,4);
+        list.add(1,10);
+        list.add(2,2);
+        list.add(9,1);
+        list.add(4,5);
+        list.add(3,7);
+        list.add(5,3); 
+        list.add(5,2);
+        list.add(0,9);
+        list.add(2,0);
+        list.add(4,13);
+        
+        
+        
+        
+        list.printList();
+//        System.out.println(list.size());
+        System.out.println(list.isEmpty());
+        
+    }
+}
