a1213b3c2d46923649ba66d9dea06663d01bf4c4
Jonah <Jonah@jonahscomputer>
2015-11-16 21:52:17 -0500

    done.

--- src/StringStack.java
+++ src/StringStack.java
@@ -9,19 +9,27 @@
  */
 public class StringStack {
 
+    private NodeChar first;
     private Stack words;
 
-    
     public StringStack() {
         words = new Stack();
+        first = null;
     }
-    
-    public void word(String w){
-        
-        for(int i = 1; i < w.length(); i++){
-             char b = w.charAt(0);
-             words.push(b);
-             
+
+    public void word(String w) {
+
+        for (int i = 0; i < w.length(); i++) {
+            char b = w.charAt(i);
+            words.push(b);
+        }
+
+        for (int i = 0; i < w.length(); i++) {
+            char place = words.peek();
+            for (int j = 0; j < w.length() - 1; j++) {
+                char b = w.charAt(i);
+                words.pop();
+            }
         }
     }
 }
