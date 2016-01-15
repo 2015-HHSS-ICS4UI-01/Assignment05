9f7c13783f639ba5aa8d7f5c58fd2c57dd90b009
rainy <rainy@DESKTOP-PA8C44T>
2016-01-15 11:09:40 -0500

    Merge origin/master
    Conflicts:
    	src/hhssadventure/AdventureInterface.form
    	src/hhssadventure/AdventureInterface.java
    	src/hhssadventure/HHSSAdventure.java

--- src/OrderedList.java
+++ src/OrderedList.java
@@ -21,17 +21,58 @@ public class OrderedList {
     
     public void add(int n){
         Node node = new Node(n);
-        Node temp;
-        if(numItems==0){
+        Node temp=head;
+        if(numItems==0 || node.getNum()<temp.getNum()){
+            node.setNext(temp);
+            temp.setPrev(node);
             head=node;
             numItems++;
         }else{
-            
+            for (int i = 0; i <= numItems; i++) {
+                if(node.getNum() == temp.getNum()){
+                    temp.getPrev().setNext(node);
+                    node.setPrev(temp.getPrev());
+                    node.setNext(temp);
+                    temp.setPrev(node);
+                    numItems++;
+                    break;
+                }else if(node.getNum() <= temp.getNum() && temp.getNext() == null){
+                    temp.setNext(node);
+                    node.setPrev(temp);
+                    node.setNext(null);
+                    numItems++;
+                    break;
+                }else{
+                    temp=temp.getNext();
+                }    
+            }
         }
     }
     
     public void remove(int n){
-        
+        Node node = new Node(n);
+        Node temp = head;
+        if(node.getNum() == temp.getNum()){
+            temp.getNext().setPrev(null);
+            head=temp.getNext();
+            numItems--;
+        }else{
+            for (int i = 0; i < numItems; i++) {
+                if(node.getNum() == temp.getNum()){
+                    temp.getPrev().setNext(temp.getNext());
+                    temp.getNext().setPrev(temp.getPrev());
+                    numItems--;
+                    break;
+                }else if(node.getNum() == temp.getNum() && temp.getNext() == null){
+                    temp.getPrev().setNext(null);
+                    numItems--;
+                    break;
+                }else{
+                    temp = temp.getNext();
+                }
+                    
+            }
+        }
     }
     
     public int size(){
@@ -46,7 +87,19 @@ public class OrderedList {
         return empty;
     }
     
-    public void get(int index){
-        
+    public int get(int index){
+        Node node = head;
+        for (int i = 0; i < index; i++) {
+            node = node.getNext();
+        }
+        return node.getNum();
     }
+    
+    public void printList(){
+        Node node = head;
+        while(node != null){
+            System.out.println(node.getNum());
+            node = node.getNext();
+        }
+    }    
 }
---
+++ src/test.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author rainy
+ */
+public class test {
+    public static void main(String[] args) {
+        OrderedList list = new OrderedList();
+        list.add(5);
+        list.add(3);
+        list.add(10);
+        list.printList();
+    }
+}
